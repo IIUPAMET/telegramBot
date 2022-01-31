6e55c5b75482b137ff38b3f34aa00da074ece9d2
Mykola Filimonov <mykola.filimonov@contractors.medallia.com>
2022-01-31 23:11:52 +0200

    basic functionality

--- src/main/java/org/example/telegram/TelegramBot.java
+++ src/main/java/org/example/telegram/TelegramBot.java
@@ -1,34 +1,32 @@
 package org.example.telegram;
 
+import org.apache.log4j.Logger;
+import org.example.telegram.commands.HelpCommand;
 import org.example.telegram.commands.StartCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
 public class TelegramBot extends TelegramLongPollingCommandBot {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBot.class);
+    private final Logger logger = Logger.getLogger(TelegramBot.class);
 
     private final String BOT_NAME = "fili_mono_bot";
     private final String BOT_TOKEN = "5104647841:AAEoDU-MPEiOytwe4ankwiU63sBz5YDGjmY";
 
 
-    public TelegramBot(){
-
+    public TelegramBot() {
         register(new StartCommand("start", "Start your work with bot"));
+        register(new HelpCommand("help", "Give more info to user"));
     }
+
     @Override
     public String getBotUsername() {
         return BOT_NAME;
@@ -36,13 +34,7 @@ public class TelegramBot extends TelegramLongPollingCommandBot {
 
     @Override
     public void processNonCommandUpdate(Update update) {
-
-        Message msg = update.getMessage();
-        Long chatId = msg.getChatId();
-        String userName = getUserName(msg);
-
-        String answer = String.format("Hello %s, thanks for your message:\n\"%s\"", userName, msg.getText());
-        setAnswer(chatId, userName, answer);
+        logger.debug("new Update received");
     }
 
     @Override
--- src/main/java/org/example/telegram/commands/HelpCommand.java
+++ src/main/java/org/example/telegram/commands/HelpCommand.java
@@ -1,13 +1,12 @@
 package org.example.telegram.commands;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
 public class HelpCommand extends AbstractCommand {
-    Logger logger = LoggerFactory.getLogger(HelpCommand.class);
+    Logger logger = Logger.getLogger(HelpCommand.class);
 
     public HelpCommand(String commandIdentifier, String description) {
         super(commandIdentifier, description);
@@ -15,12 +14,11 @@ public class HelpCommand extends AbstractCommand {
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
-        String userName = user.getUserName() + user.getFirstName() + user.getLastName();
-        logger.info(String.format("User: %s. start executing command %s", userName,
+        logger.info(String.format("User: %s. start executing command %s", user.getId(),
                 this.getCommandIdentifier()));
-        sendAnswer(absSender, chat.getId(), this.getCommandIdentifier(), userName,
+        sendAnswer(absSender, chat.getId(), this.getCommandIdentifier(), user.getUserName(),
                 "Давайте начнём! Если Вам нужна помощь, нажмите /help");
-        logger.info(String.format("Пользователь %s. Завершено выполнение команды %s", userName,
+        logger.info(String.format("User: %s. end executing command %s", user.getId(),
                 this.getCommandIdentifier()));
     }
 }
